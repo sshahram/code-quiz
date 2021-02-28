17575a6442074083e13dc97c521558669e2fc93e
sshahram <shirin.shahram23@gmail.com>
2021-02-28 00:20:21 -0800

    print high scores to the screen

---
+++ assets/js/highscores.js
@@ -0,0 +1,23 @@
+var printScores = function () {
+    var highScore = JSON.parse(window.localStorage.getItem("highscore")) || [];
+    highScore.sort(function(x, y){
+        return y.score - x.score
+    });
+
+    highScore.forEach(function(score) {
+        var scoreEl = document.createElement("li");
+        scoreEl.textContent = score.initial + " - " + score.score;
+    
+        var scoreListEl = document.getElementById("highscore");
+        scoreListEl.appendChild(scoreEl);
+      });
+    }
+    
+function clearScores() {
+      window.localStorage.removeItem("highScore");
+      window.location.reload();
+    }
+    
+    document.getElementById("clear").onclick = clearScores;
+    
+    printScores();
